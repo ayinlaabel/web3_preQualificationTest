2ee30693ba72c1852592a6825d5a6118e1e28362
ayinlaabel <kraneabel@gmail.com>
2022-05-22 10:15:14 +0100

    Deployment

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 const app = express();
 
 
-app.use(express.static(__dirname + "/dist/taskmanager"));
+app.use(express.static(__dirname + "/dist/web3"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/taskmanager/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/web3/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
