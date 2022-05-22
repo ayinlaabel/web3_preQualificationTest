71b32c37cd6314698de48e9d2d9dd28330edeaf2
ayinlaabel <kraneabel@gmail.com>
2022-05-22 10:16:36 +0100

    Deployment

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-
 app.use(express.static(__dirname + "/dist/web3"));
 
 app.get("/*", function (req, res) {
